4d9ebc50a16d3773c8b90038ef2cfbbcb92a7c95
Ayush Kumar <ayushpal2017ak@gmail.com>
2024-07-10 13:45:23 +0530

    Update 1.4.js

--- Week-1/1.4.js
+++ Week-1/1.4.js
@@ -1,3 +1,4 @@
+
 function findSum(n){
   let ans=0;
   for(let i=1;i<n;i++){
@@ -23,3 +24,59 @@ fs.readFile("a.txt","utf-8", function(err, data){
 })
 
 console.log("hi there its Ayush");
+
+const fs= require("fs");
+
+//my own asynchronous function
+
+function kiratsReadFile(){
+    console.log("inside KiratReadFile");
+    return new Promise(function(resolve){
+        console.log("inside Promise")
+        fs.readFile("a.txt","utf-8", function(err, data){
+            console.log("Before Resolve");
+            resolve(data);
+        });
+    })
+}
+
+//Callback function to call
+
+function onDone(data) {
+    console.log(data)
+}
+
+kiratsReadFile().then(onDone);
+
+
+
+var d=new Promise(function(resolve){
+
+});
+console.log(d);
+
+// Async Await 
+
+function KiratsAsyncFunction(){
+    let p = new Promise(function(resolve){
+        
+        // DO some async logic  here
+        setTimeout(function(){
+            resolve("Hi there!");
+        }, 3000)
+        
+    });
+    return p;
+}
+
+ async function main(){
+     console.log("Hi there2")
+    let value = await KiratsAsyncFunction()
+        console.log(value);
+        console.log("hi there1")
+    
+}
+main();
+console.log("after main");
+
+ 
