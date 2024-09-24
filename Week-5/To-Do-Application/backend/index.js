5aa30771f60431e45ea38b83a7e790aee21af163
Ayush Kumar <ayushpal2017ak@gmail.com>
2024-09-24 23:57:23 +0530

    added zod validation

--- Week-5/To-Do-Application/backend/index.js
+++ Week-5/To-Do-Application/backend/index.js
@@ -3,12 +3,21 @@
 
 
 const express = require("express")
+const { createTodo, updateTodo } = require("./types");
 const app = express();
 
 app.use(express.json);
 
 app.post("todo", function(req, res) {
-
+    const createPayload = req.body;
+    const parsedPayload = createTodo.safeParse(createPayload);
+    if(!parsedPayload.success) {
+        res.status(411).json({
+            msg: "You sent the wrong inputs",
+        })
+        return;
+    }
+    // Put it in mongoDB
 })
 
 app.get("todos", function(req, res){
@@ -16,5 +25,12 @@ app.get("todos", function(req, res){
 })
 
 app.put("/completed", function(req, res){
-    
+    const updatePayload = req.body;
+    const parsedPayload = updateTodo.safeParse(updatePayload);
+    if(!parsedPayload.success) {
+        res.status(411).json({
+            msg: "You sent the wrong inputs",
+        })
+        return;
+    }
 })
