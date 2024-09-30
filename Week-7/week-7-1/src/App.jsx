e545c5342faba48e0c03e6f6c2d531dadc692cbd
Ayush Kumar <ayushpal2017ak@gmail.com>
2024-09-30 22:44:32 +0530

    ContextAPI

--- Week-7/week-7-1/src/App.jsx
+++ Week-7/week-7-1/src/App.jsx
@@ -1,9 +1,13 @@
 import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom"
-import { Suspense, lazy } from "react"
+import { Suspense, lazy, useContext, useState } from "react"
 import './App.css'
+import { set } from "zod"
 const Dashboard = lazy(() => import("./components/Dashboard"))
 const   Landing = lazy(() => import("./components/Landing"))
 
+import { CountContext } from "./context";
+
+/*
 function App() {
   return (
     <div>
@@ -36,6 +40,51 @@ function Appbar() {
           }}>Dashboard</button>  
      </div>
      </div>)
+}
+
+*/
+
+function App() {
+
+  const [count, setCount]= useState(0);
+
+  //wrap anyone that wants to use the teleported value inside a provider
+  return (
+    <div>
+       <CountContext.Provider value={count}>
+        <Count setCount={setCount} />
+       </CountContext.Provider>
+    </div>
+  )
+}
+
+function Count({setCount}) {
+  return <div>
+    <CountRenderer />
+    <Buttons setCount={setCount} />
+  </div>
+}
+
+function CountRenderer() {
+  const count = useContext(CountContext);
+
+  return <div>
+    {count}
+  </div>
+}
+
+function Buttons({setCount}) {
+const count = useContext(CountContext);
+
+  return <div>
+    <button onClick={()=> {
+      setCount(count+1)
+    }}>Increase</button>
+
+    <button onClick={()=> {
+      setCount(count-1)
+    }}>Decrease</button>
+  </div>
 }
 
 export default App
