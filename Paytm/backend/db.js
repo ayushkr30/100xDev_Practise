95a2db3f9eccbe554582e43fcd6ffc2426205f35
Ayush Kumar <ayushpal2017ak@gmail.com>
2024-10-04 02:08:08 +0530

    added transaction functionality

--- Paytm/backend/db.js
+++ Paytm/backend/db.js
@@ -35,6 +35,21 @@ const userSchema = new mongoose.Schema({
 //Create a model from the Schema
 const User = mongoose.model('User', userSchema);
 
+const accountSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    balance : {
+        type: Number,
+        required: true
+    }
+});
+
+const Account = mongoose.model('Account', accountSchema);
+
+
 module.exports = {
     User
 };
