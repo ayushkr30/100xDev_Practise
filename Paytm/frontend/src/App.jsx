6fd0804e3a54455e1f6a96a6b9231e6b9403348e
Ayush Kumar <ayushpal2017ak@gmail.com>
2024-10-04 21:28:07 +0530

    payment app completed

--- Paytm/frontend/src/App.jsx
+++ Paytm/frontend/src/App.jsx
@@ -1,3 +1,13 @@
+import {
+  BrowserRouter,
+  Route,
+  Routes,
+} from "react-router-dom";
+import { Signup } from "./pages/Signup";
+import { Signin } from "./pages/Signin";
+import { Dashboard } from "./pages/Dashboard";
+import { SendMoney } from "./pages/SendMoney";
+
 
 function App() {
 
@@ -5,7 +15,7 @@ function App() {
     <>
     <BrowserRouter>
     <Routes>
-      <Route path="/signup" element={<Signup/}/>
+      <Route path="/signup" element={<Signup/>}/>
       <Route path="signin" element={<Signin/>}/>
       <Route path="/dashboard" element={<Dashboard/>}/>
       <Route path="/send" element={<SendMoney/>}/>
